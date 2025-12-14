41e2a9f745091a8abdf4388d144b4deec19461f8
Tyler <davistglenn@gmail.com>
2025-12-14 02:17:04 -0500

    Alter the design of the login portion of the launcher

--- WoW.Launcher/Form1.Designer.cs
+++ WoW.Launcher/Form1.Designer.cs
@@ -30,146 +30,180 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
-            panelRegister = new Panel();
-            textBox3 = new TextBox();
-            label5 = new Label();
-            button2 = new Button();
-            button1 = new Button();
-            textBox2 = new TextBox();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            label1 = new Label();
             labelGameVer = new Label();
             labelLauncherVer = new Label();
+            splitContainer1 = new SplitContainer();
+            linkLabelGoToLogin = new LinkLabel();
+            checkBoxOfflineMode = new CheckBox();
+            buttonRegister = new Button();
+            textBoxEmail = new TextBox();
+            label3 = new Label();
+            textBoxPassword = new TextBox();
+            label2 = new Label();
+            textBoxAccountName = new TextBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            panelRegister.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(120, 22);
+            pictureBox1.Location = new Point(152, 12);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(66, 67);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // panelRegister
-            // 
-            panelRegister.BorderStyle = BorderStyle.Fixed3D;
-            panelRegister.Controls.Add(textBox3);
-            panelRegister.Controls.Add(label5);
-            panelRegister.Controls.Add(button2);
-            panelRegister.Controls.Add(button1);
-            panelRegister.Controls.Add(textBox2);
-            panelRegister.Controls.Add(label2);
-            panelRegister.Controls.Add(textBox1);
-            panelRegister.Controls.Add(label1);
-            panelRegister.Dock = DockStyle.Bottom;
-            panelRegister.Location = new Point(0, 163);
-            panelRegister.Name = "panelRegister";
-            panelRegister.Size = new Size(307, 170);
-            panelRegister.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(126, 82);
-            textBox3.Name = "textBox3";
-            textBox3.PlaceholderText = "Optional";
-            textBox3.Size = new Size(167, 27);
-            textBox3.TabIndex = 7;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(10, 85);
-            label5.Name = "label5";
-            label5.Size = new Size(49, 20);
-            label5.TabIndex = 6;
-            label5.Text = "Email:";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(10, 127);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 5;
-            button2.Text = "<- Back";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(199, 127);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 4;
-            button1.Text = "Register";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(126, 49);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(167, 27);
-            textBox2.TabIndex = 3;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(10, 52);
-            label2.Name = "label2";
-            label2.Size = new Size(73, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Password:";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(126, 16);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(167, 27);
-            textBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(10, 19);
-            label1.Name = "label1";
-            label1.Size = new Size(110, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Account Name:";
-            // 
             // labelGameVer
             // 
             labelGameVer.AutoSize = true;
-            labelGameVer.Location = new Point(0, 120);
+            labelGameVer.Location = new Point(0, 129);
             labelGameVer.Name = "labelGameVer";
             labelGameVer.Size = new Size(77, 20);
             labelGameVer.TabIndex = 2;
             labelGameVer.Text = "Game v{0}";
+            labelGameVer.Click += labelGameVer_Click;
             // 
             // labelLauncherVer
             // 
             labelLauncherVer.AutoSize = true;
-            labelLauncherVer.Location = new Point(0, 140);
+            labelLauncherVer.Location = new Point(0, 149);
             labelLauncherVer.Name = "labelLauncherVer";
             labelLauncherVer.Size = new Size(97, 20);
             labelLauncherVer.TabIndex = 3;
             labelLauncherVer.Text = "Launcher v{0}";
             // 
+            // splitContainer1
+            // 
+            splitContainer1.BorderStyle = BorderStyle.Fixed3D;
+            splitContainer1.Dock = DockStyle.Bottom;
+            splitContainer1.Location = new Point(0, 172);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(linkLabelGoToLogin);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(checkBoxOfflineMode);
+            splitContainer1.Panel2.Controls.Add(buttonRegister);
+            splitContainer1.Panel2.Controls.Add(textBoxEmail);
+            splitContainer1.Panel2.Controls.Add(label3);
+            splitContainer1.Panel2.Controls.Add(textBoxPassword);
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(textBoxAccountName);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Size = new Size(364, 208);
+            splitContainer1.SplitterDistance = 163;
+            splitContainer1.TabIndex = 4;
+            // 
+            // linkLabelGoToLogin
+            // 
+            linkLabelGoToLogin.AutoSize = true;
+            linkLabelGoToLogin.Location = new Point(20, 86);
+            linkLabelGoToLogin.Name = "linkLabelGoToLogin";
+            linkLabelGoToLogin.Size = new Size(126, 20);
+            linkLabelGoToLogin.TabIndex = 0;
+            linkLabelGoToLogin.TabStop = true;
+            linkLabelGoToLogin.Text = "Have an account?";
+            linkLabelGoToLogin.LinkClicked += linkLabelGoToLogin_LinkClicked;
+            // 
+            // checkBoxOfflineMode
+            // 
+            checkBoxOfflineMode.AutoSize = true;
+            checkBoxOfflineMode.Location = new Point(14, 119);
+            checkBoxOfflineMode.Name = "checkBoxOfflineMode";
+            checkBoxOfflineMode.Size = new Size(121, 24);
+            checkBoxOfflineMode.TabIndex = 7;
+            checkBoxOfflineMode.Text = "Offline-mode";
+            checkBoxOfflineMode.UseVisualStyleBackColor = true;
+            checkBoxOfflineMode.CheckedChanged += OnOfflineModeChanged;
+            // 
+            // buttonRegister
+            // 
+            buttonRegister.Location = new Point(91, 172);
+            buttonRegister.Name = "buttonRegister";
+            buttonRegister.Size = new Size(94, 29);
+            buttonRegister.TabIndex = 6;
+            buttonRegister.Text = "Register";
+            buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.Location = new Point(14, 139);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.PlaceholderText = "Optional";
+            textBoxEmail.Size = new Size(171, 27);
+            textBoxEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Email";
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(14, 86);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.Size = new Size(171, 27);
+            textBoxPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Password";
+            // 
+            // textBoxAccountName
+            // 
+            textBoxAccountName.Location = new Point(14, 33);
+            textBoxAccountName.Name = "textBoxAccountName";
+            textBoxAccountName.Size = new Size(171, 27);
+            textBoxAccountName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Account Name";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(307, 333);
+            ClientSize = new Size(364, 380);
+            Controls.Add(splitContainer1);
             Controls.Add(labelLauncherVer);
             Controls.Add(labelGameVer);
-            Controls.Add(panelRegister);
             Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            panelRegister.ResumeLayout(false);
-            panelRegister.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -177,16 +211,17 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private Panel panelRegister;
-        private Label label1;
-        private Button button1;
-        private TextBox textBox2;
-        private Label label2;
-        private TextBox textBox1;
-        private Button button2;
         private Label labelGameVer;
         private Label labelLauncherVer;
-        private TextBox textBox3;
-        private Label label5;
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private TextBox textBoxEmail;
+        private Label label3;
+        private TextBox textBoxPassword;
+        private Label label2;
+        private TextBox textBoxAccountName;
+        private Button buttonRegister;
+        private LinkLabel linkLabelGoToLogin;
+        private CheckBox checkBoxOfflineMode;
     }
 }
--- WoW.Launcher/Form1.cs
+++ WoW.Launcher/Form1.cs
@@ -2,9 +2,49 @@ namespace WoW.Launcher
 {
     public partial class Form1 : Form
     {
+        private bool _isLoginState = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void labelGameVer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabelGoToLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (!_isLoginState)
+            {
+                label3.Hide();
+                textBoxEmail.Hide();
+                checkBoxOfflineMode.Show();
+                buttonRegister.Text = "Login";
+                linkLabelGoToLogin.Text = "Need an account?";
+                _isLoginState = true;
+            }
+            else
+            {
+                label3.Show();
+                textBoxEmail.Show();
+                checkBoxOfflineMode.Hide();
+                buttonRegister.Text = "Register";
+                linkLabelGoToLogin.Text = "Have an account?";
+                _isLoginState = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBoxOfflineMode.Hide();
+        }
+
+        private void OnOfflineModeChanged(object sender, EventArgs e)
+        {
+            textBoxAccountName.Enabled = !checkBoxOfflineMode.Checked;
+            textBoxPassword.Enabled = !checkBoxOfflineMode.Checked;
         }
     }
 }
